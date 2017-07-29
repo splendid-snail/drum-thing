63385155c1bff5a37df33750aee5d15528b16da2
Iain Cooper <iaincoop@gmail.com>
2017-07-29 18:23:10 +0100

    fancy display stuff

--- drum_build_one.ino
+++ drum_build_one.ino
@@ -1,7 +1,7 @@
 /* TO DO **************************************************************
  * make another 'WavFiles' array with another sound set
  * The array chosen by setValues() could vary based on a rotary switch!
- * Button could enable 4-to-the-floor BD <<DO THIS *
+ * Button could enable 4-to-the-floor BD <<DO THIS. consider 'nudge' button too
  * add 'swing'?
  * LOW TOM sound (3) seems to pop at the end
  */
@@ -15,18 +15,24 @@
 #define PITCHPIN A1
 #define SWITCHPIN 3
 #define SHUFFPIN 2
+#define SEEDPIN A5
+
+//lengths of the various file arrays. keep these up to date or it breaks
+#define CGLENGTH 4
+#define CBLENGTH 13
 
 int redLed = 15; //this sets the start position
 int potVal = 0;
 int pitchVal = 0;
 int congaFiles[] = {0,1,2,3}; //these ints relate to sample numbers on the SD card; 0 is null
-int cowbellFiles[] = {0,0,4,5,6,7,8,9,10,11,12,13}; //ditto as above. Could add another null?
-int instrumentSelect = 1; //referenced by setValues() and shuffle() to determine which Files array to use. 0 = conga, 1 = cowbell. Currently has to be set here.  
+int cowbellFiles[] = {0,0,0,4,5,6,7,8,9,10,11,12,13};
+int instrumentSelect = 1; //referenced by setValues() and shuffle() to determine which Files array to use. 0 = conga, 1 = cowbell. Currently has to be set here.
 int playingNow = 1; //to prevent shuffling the two arrays together
 int stepValues[16];
 int currentStep = 0;
 int switchButtonState = 0;
 int shuffButtonState = 0;
+int switchButtonCounter = 0;
 
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, RINGPIN);
 wavTrigger wTrig;
@@ -35,12 +41,12 @@ wavTrigger wTrig;
 void setValues(){
   for (int beat =  0; beat < 16; beat++){
     if (instrumentSelect == 0){
-      stepValues[beat] = congaFiles[random(4)]; //make sure the random number is the same as the Files array length
-      playingNow = 0;  
+      stepValues[beat] = congaFiles[random(CGLENGTH)]; //make sure the random number is the same as the Files array length
+      playingNow = 0;
     }
     else {
-      stepValues[beat] = cowbellFiles[random(12)];
-      playingNow = 1;    
+      stepValues[beat] = cowbellFiles[random(CBLENGTH)];
+      playingNow = 1;
     }
   }
 }
@@ -48,16 +54,15 @@ void setValues(){
 
 //shuffles the current pattern obvs
 void shuffle(){
-  int shuffSeed; //redundant?
   for (int beat = 0; beat <16; beat++){
     int shuffSeed = random(0, 5); //still looking for the best value but this is ok
-    if (shuffSeed < 1){      
+    if (shuffSeed < 1){
       if (playingNow == 0){
-        stepValues[beat] = congaFiles[random(4)]; //make sure the random number is the same as the Files array length  
+        stepValues[beat] = congaFiles[random(CGLENGTH)]; //make sure the random number is the same as the Files array length
       }
-      else {
-        stepValues[beat] = cowbellFiles[random(12)];    
-      }    
+      else{
+        stepValues[beat] = cowbellFiles[random(CBLENGTH)];
+      }
     }
   }
 }
@@ -67,7 +72,7 @@ void setup() {
   pinMode(SWITCHPIN, INPUT_PULLUP);
   pinMode(SHUFFPIN, INPUT_PULLUP);
   Serial.begin(9600);
-  randomSeed(analogRead(5)); //make sure this pin isn't connected to anything
+  randomSeed(analogRead(SEEDPIN)); //make sure this pin isn't connected to anything
   ring.begin();
   ring.setBrightness(127);
   ring.show(); // init pixels to 'off'
@@ -87,7 +92,10 @@ void loop() {
     setValues();
     redLed = 15;
     currentStep = 0;
-  }
+	switchButtonCounter++;
+} else{
+	switchButtonCounter = 0;
+}
 
   if (shuffButtonState == 0){
     shuffle();
@@ -98,43 +106,57 @@ void loop() {
     currentStep = 0;
   }
 
-  //Set all pixels to blue
-  for (int i = 0; i < 16; i++) {
+
+  //RING STUFF
+  if (switchButtonCounter < 1 ){ //step red LED in normal mode
+	  if (redLed < 0){
+	    redLed = 15;
+	  }
+    for (int i = 0; i < 16; i++) {
      ring.setPixelColor(i, 0, 0, 255);
+    }
+    ring.setPixelColor(redLed, 255, 0, 0);
+	  redLed--;
   }
-
-  //red led bits
-  if (redLed < 0){
-    redLed = 15;
+  else{ //flash random colours if holding switch button
+	  if (switchButtonCounter > 0){
+      if (stepValues[currentStep] > 0){
+        int rand1 = random(0, 256);
+        int rand2 = random(0, 256);
+        int rand3 = random(0, 256);
+        for (int i = 0; i < 16; i++){
+          ring.setPixelColor(i, rand1, rand2, rand3);
+        }
+	    }
+      else{
+        for (int i = 0; i < 16; i++){
+          ring.setPixelColor(i, 0, 0, 0);
+        }
+      }
+	  }
   }
-
-  ring.setPixelColor(redLed, 255, 0, 0);
-
-  redLed--;
   ring.show();
 
-  //read potval, 0-1023
+  //read pots 
   potVal = analogRead(SPEEDPIN);
-  //Serial.println(potVal);
-  //map potval
   potVal = map(potVal, 0, 1023, 80, 600); //tweak latter values for min/max step length
 
-  //read pitchval and set
   pitchVal = analogRead(PITCHPIN);
-  //Serial.println(pitchVal);
   pitchVal = map(pitchVal, 0, 1023, 32676, -32767);
   wTrig.samplerateOffset(pitchVal);
 
   //play a sample
-  wTrig.trackPlaySolo(stepValues[currentStep]); // try PlaySolo or PlayPoly
+  wTrig.trackPlayPoly(stepValues[currentStep]); //PlayPoly is def better with cowbells. Congas may prefer PlaySolo
   wTrig.update();
 
   //Serial debug stuff
   Serial.println(potVal);
   Serial.print("Step ");
   Serial.println(currentStep);
-  Serial.print("Playing file ");
-  Serial.println(stepValues[currentStep]);
+  Serial.print("Hold count: ");
+  Serial.println(switchButtonCounter);
+  //Serial.print("Playing file ");
+  //Serial.println(stepValues[currentStep]);
   //Serial.println(switchButtonState);
 
   //increment step and delay
